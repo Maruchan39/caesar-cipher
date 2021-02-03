e77c4f13163ba40c0a2b2c347e6154242338bc77
Martynas Baltutis <66354349+Maruchan39@users.noreply.github.com>
2021-02-03 23:24:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function rot13(str) {
-    let result = [];
-    for (let i = 0; i < str.length; i++) {
-        if (/[^a-z]/i.test(str[i])) {
-            result.push(str[i]);
-        }
-        else if (str.charCodeAt(i) + 13 <= 90) {
-            result.push(String.fromCharCode(str.charCodeAt(i) + 13));
-        }
-        else {
-            result.push(String.fromCharCode(str.charCodeAt(i) - 13));
-        }
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (/[^a-z]/i.test(str[i])) {
+      result.push(str[i]);
+    } else if (str.charCodeAt(i) + 13 <= 90) {
+      result.push(String.fromCharCode(str.charCodeAt(i) + 13));
+    } else {
+      result.push(String.fromCharCode(str.charCodeAt(i) - 13));
     }
-    return result.join('');
-
+  }
+  return result.join("");
 }
 
 
